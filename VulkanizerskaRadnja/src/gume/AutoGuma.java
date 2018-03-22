eb6ba6a2e77d3aae55c00ddf43a855035f4580ad
MutexOrator <milosevic10116@fon.bg.ac.rs>
2018-03-22 10:59:25 +0100

    Dodata dokumentacija u klasi AutoGuma

--- VulkanizerskaRadnja/src/gume/AutoGuma.java
+++ VulkanizerskaRadnja/src/gume/AutoGuma.java
@@ -1,45 +1,106 @@
 package gume;
-
+/**
+ * 
+ * @author Dusko Milosevic
+ * @version 1.0
+ *
+ */
 public class AutoGuma {
+	/**
+	 * Marka modela
+	 */
 	private String markaModel = null;
+	/**
+	 * Precnik gume
+	 */
 	private int precnik = 0;
+	/**
+	 * Sirina gume
+	 */
 	private int sirina = 0;
+	/**
+	 * Visina gume
+	 */
 	private int visina = 0;
-
+	/**
+	 * Vraca vrednost atributa za marku modela
+	 * @return marku modela kao string
+	 */
 	public String getMarkaModel() {
 		return markaModel;
 	}
-
+	/**
+	 * Postavlja novu vrenost za atribut marka modela
+	 * @param markaModel nova marka modela
+	 * @throws java.lang.RuntimeExeption ako je unet
+	 * <ul>
+	 * <li>null</li>
+	 * <li>String kraci od 3 karaktera</li>
+	 * </ul>
+	 */
 	public void setMarkaModel(String markaModel) {
 		if (markaModel == null || markaModel.length() < 3)
 			throw new RuntimeException("Morate uneti marku i model");
 		this.markaModel = markaModel;
 	}
-
+	/**
+	 * Vraca vrednost za precnik gume
+	 * @return precnik gume kao String
+	 */
 	public int getPrecnik() {
 		return precnik;
 	}
-
+	/**
+	 * Postavlja novu vrednost za atribut precnik gume
+	 * @param precnik novi precnik gume
+	 * @throws java.lang.RuntimeExeption ako je unet
+	 * <ul>
+	 * <li>null</li>
+	 * <li>String kraci od 3 karaktera</li>
+	 * </ul>
+	 */
 	public void setPrecnik(int precnik) {
 		if (precnik < 13 && precnik > 22)
 			throw new RuntimeException("Precnik van opsega");
 		this.precnik = precnik;
 	}
-
+	/**
+	 * Vraca vrednost za sirinu gume
+	 * @return sirina gume kao String
+	 */
 	public int getSirina() {
 		return sirina;
 	}
-
+	/**
+	 * Postavlja novu vrednost za atribut sirina gume
+	 * @param sirina nova sirina gume
+	 * @throws java.lang.RuntimeExeption ako je unet
+	 * <ul>
+	 * <li>null</li>
+	 * <li>String kraci od 3 karaktera</li>
+	 * </ul>
+	 */
 	public void setSirina(int sirina) {
 		if (sirina < 135 && sirina > 355)
 			throw new RuntimeException("Sirina van opsega");
 		this.sirina = sirina;
 	}
-
+	/**
+	 * Vraca vrednost za visinu gume
+	 * @return visina gume kao String
+	 */
 	public int getVisina() {
 		return visina;
 	}
-
+	/**
+	 * Postavlja novu vrednost za atribut visina gume
+	 * @param visina nova visina gume
+	 *  @throws java.lang.RuntimeExeption ako je unet
+	 * <ul>
+	 * <li>null</li>
+	 * <li>String kraci od 3 karaktera</li>
+	 * </ul>
+	 */
 	public void setVisina(int visina) {
 		if (visina < 25 || visina > 95)
 			throw new RuntimeException("Visina van opsega");
